f21ccb96b3a01e8d766ba933fe596e522a0a32c6
smkhalsa <smkhalsa@gmail.com>
2015-06-15 15:58:42 -0700

    Add multiple columns for subnodes

--- app/canvas/canvas.js
+++ app/canvas/canvas.js
@@ -1,7 +1,15 @@
 angular.module('app.canvas', ['firebase'])
 
   .controller('CanvasController', function($scope, $firebaseArray) {
-    var nodeRef = new Firebase("https://resolver.firebaseio.com/nodes");
 
-    $scope.nodes = $firebaseArray(nodeRef);
+    var nodeRef = new Firebase("https://resolver.firebaseio.com/nodes");
+    var rootNodes = $firebaseArray(nodeRef);
+    $scope.current = 'rootNodes[0]';
+    $scope.setCurrent = function(node, colIndex) {
+      console.log(colIndex);
+      $scope.current = node;
+      $scope.columns.splice(colIndex+1);
+      $scope.columns.push(node.children);
+    }
+    $scope.columns = [rootNodes];
   })
