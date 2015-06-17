755e60df8ff6c1516c134ea06e49dc11871035b3
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-06-16 21:26:06 -0700

    Implemented callback for getRootNode to replace setTimeout

--- app/canvas/canvas.js
+++ app/canvas/canvas.js
@@ -4,11 +4,9 @@ angular.module('resolver.canvas', [
     'firebase'
   ])
 
-  .controller('CanvasController', function($scope, $state, $rootScope, $stateParams, Nodes, $firebaseArray) {
+  .controller('CanvasController', function($scope, $state, $rootScope, $stateParams, Nodes) {
 
     $rootScope.$state = $state;
-
-    var rootNodes = [Nodes.getNodeChildren('/')];
 
     $scope.rootNode = $stateParams.id;
     $scope.colPaths = ['']
@@ -17,25 +15,22 @@ angular.module('resolver.canvas', [
     $scope.currentNode;
     $scope.currentCol;
 
-    setTimeout(function(){
-      for(var i = 0; i < rootNodes[0].length; i++ ) {
-        if(rootNodes[0][i].$id === $scope.rootNode) {
-          $scope.columns = [[rootNodes[0][i]]];
-          $scope.setCurrent($scope.columns[0][0], 0, 0)
-        }
-      }
-    },1000)
+    Nodes.getRootNode($scope.rootNode, function(node) {
+      console.log(node);
+      $scope.columns = [[node]];
+      $scope.setCurrent($scope.columns[0][0], 0, 0);
+    });
 
     $scope.addItem = function(nodeTitle){
       var parentPath = $scope.colPaths.slice().join('');
-      Nodes.addChild(nodeTitle, parentPath)
-    }
+      Nodes.addChild(nodeTitle, parentPath);
+    };
 
     $scope.removeItem = function(node, index, colIndex){
       var parentPath = $scope.colPaths.slice(0,colIndex+1).join('');
-      console.log(parentPath +"/"+ node.$id)
+      console.log(parentPath +"/"+ node.$id);
       Nodes.removeNode(parentPath+"/"+node.$id);
-    }
+    };
 
     $scope.setCurrent = function(node, index, colIndex) {
       $scope.currentCol = colIndex;
@@ -45,6 +40,6 @@ angular.module('resolver.canvas', [
       $scope.colPaths.push('/'+node.$id+'/children');
       $scope.columns.splice(colIndex+1);
       $scope.columns.push(Nodes.getNodeChildren($scope.colPaths.join('')));
-    }
+    };
 
-  })
+  });
--- app/services/services.js
+++ app/services/services.js
@@ -8,32 +8,31 @@ angular.module('resolver.services', [
 
     Nodes.getNodeChildren = function(path) {
       return $firebaseArray(new Firebase("https://resolver.firebaseio.com/nodes" + path));
-    }
+    };
 
-    // Nodes.getRootNode = function(nodeId) {
-    //   var rootNodes = $firebaseArray(new Firebase("https://resolver.firebaseio.com/nodes"));
-    //   rootNodes.$loaded()
-    //     .then(function(nodes){
-    //       console.log('nodes: ', nodes)
-    //       for (var i = 0; i < rootNodes.length; i++) {
-    //         if(rootNodes[i].$id === nodeId) {
-    //           CanvasController.columns=[rootNodes[i]]
-    //         }
-    //       }
-    //     })
-    // }
+    Nodes.getRootNode = function(nodeId, callback) {
+      var rootNodes = $firebaseArray(new Firebase("https://resolver.firebaseio.com/nodes"));
+      rootNodes.$loaded()
+        .then(function(nodes){
+          for (var i = 0; i < nodes.length; i++) {
+            if(nodes[i].$id === nodeId) {
+              callback(nodes[i]);
+            }
+          }
+        });
+    };
 
     // adds a child and returns the child's ID
     Nodes.addChild = function(childTitle, parentPath){
       var ref = new Firebase("https://resolver.firebaseio.com/nodes" + parentPath);
       // return child ID
       return ref.push({title: childTitle}).key();
-    }
+    };
 
     Nodes.removeNode = function(path){
       var ref = new Firebase("https://resolver.firebaseio.com/nodes" + path)
       ref.remove();
-    }
+    };
 
     return Nodes;
-  })
+  });
